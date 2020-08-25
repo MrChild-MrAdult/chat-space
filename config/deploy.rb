bba78a232500937af7bca4bcade656d5fc4020d6
MrChild-MrAdult <mr.child0729@gmail.com>
2020-08-25 22:14:09 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '~>3.14.1'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:MrChild-MrAdult/chat-spacegit'
+set :repo_url,  'git@github.com:MrChild-MrAdult/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
